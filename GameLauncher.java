aac4e533b79918d78fbf972471b873bf6c61e701
15010416 <43272517+15010416@users.noreply.github.com>
2018-09-20 15:11:48 +0100

    Add files via upload

---
+++ GameLauncher.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class GameLauncher {
+    public static void main (String[] args) {
+        Guessing_Game game = new Guessing_Game();
+        game.startGame();
+    }
+}
